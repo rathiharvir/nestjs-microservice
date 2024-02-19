19515861f152c4867e142f91ba256d808d1743b2
rathiharvir <rathiharvir@gmail.com>
2024-02-19 14:15:30 +0530

    Base Setup

---
+++ apps/nestjs-microservice/src/reservations/reservations.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+
+import { ReservationsRepository } from './reservations.repository';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+
+@Injectable()
+export class ReservationsService {
+  constructor(
+    private readonly reservationsRepository: ReservationsRepository,
+  ) {}
+
+  async create(createReservationDto: CreateReservationDto) {
+    return this.reservationsRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123', // for now just hard-code it
+    });
+  }
+
+  async findAll() {
+    return this.reservationsRepository.find({});
+  }
+
+  async findOne(_id: string) {
+    return this.reservationsRepository.findOne({ _id });
+  }
+
+  async update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationsRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  async remove(_id: string) {
+    return this.reservationsRepository.findOneAndDelete({ _id });
+  }
+}
